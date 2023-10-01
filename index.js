2d15f75de22270c17d67664ac3ab939fbd36f9d2
Aashutosh Rathi <aashutoshrathi@gmail.com>
2023-10-01 15:50:02 +0530

    Update for Nibble 3^3

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ const getLatestNibble = async () => {
 
 setTimeout(async () => {
  // const latestUrl = await getLatestNibble();
-  const latestUrl = "https://thenibble.substack.com/p/26";
+  const latestUrl = "https://thenibble.substack.com/p/27";
   window.location.href = latestUrl;
 }, 1000);
