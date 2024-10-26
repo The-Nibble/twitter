26615aa41964b99bbaf639dafbcdb2d6db475c61
pushkar <git@thepushkarp.com>
2024-10-26 15:32:16 +0530

    Automate updating the site with the latest edition's link (#77)
    * Implemented dynamic fetching of the latest Nibble from the RSS feed.
    
    * Updated title and metadata to "The Nibble" and changed social media images.
    
    * Refactor fetchLatestNibble to use rss2json API.
    
    * remove autoupdating action
    
    * feat: Update Nibble link every 6 hours
    
    - Update the Nibble link and number every 6 hours using a cron job.
    - Remove the pull request creation step from the workflow.
    - Add error handling for fetching the RSS feed and extracting the nibble number.
    - Update files using the new link and nibble number.
    - Commit and push changes if any.
    - Use `node-fetch` to fetch data and `child_process.execSync` to interact with Git.
    - Store the current link and nibble number in `current_link.txt`.
    
    * feat: Automate Nibble link update
    
    - Update `index.html` and `wallpaper.html` with the latest Nibble number and link.
    - Add `current_link.txt` to store the latest link and number.
    - Use regexes to update the relevant parts of the files.
    - Improve error handling.
    - Add package-lock.json and package.json.
    - Update the update-nibble.js script to handle multiple regexes.
    - Commit and push changes using git.
    
    * feat: Add .gitignore for Node.js projects
    
    * feat: Automate weekly Nibble content checklist creation
    
    - Created a GitHub Actions workflow to automatically create a weekly issue for tracking Nibble content.
    - The workflow runs every Thursday at 3:14 PM IST.
    - Updated the `updateNibble.js` script to improve the efficiency and clarity of file updates.
    - The `updateNibble.js` script now only updates files if changes are detected.
    - Updated the `update-nibble.yml` workflow to use the latest versions of the actions and to push changes directly to the repository, rather than using a pull request.
    
    * reduce udpate interval to 3 hours
    
    * fix some stale numbers
    
    * chore: update issue time to saturday to accomodate mid-week publishing schedule
    
    * minor refactor
    
    ---------
    
    Co-authored-by: Aashutosh Rathi <aashutoshrathi@gmail.com>

---
+++ updateNibble.js
@@ -0,0 +1,145 @@
+import fs from 'fs';
+import fetch from 'node-fetch';
+
+const RSS2JSON_ENDPOINT = 'https://api.rss2json.com/v1/api.json?rss_url=';
+const FEED_URL = encodeURIComponent('https://www.nibbles.dev/feed');
+const API_URL = `${RSS2JSON_ENDPOINT}${FEED_URL}`;
+const DELIMITER = '|';
+
+// Files to update
+const filesToUpdate = ['index.html', 'wallpaper.html'];
+
+// Regexes to update
+const regexesToUpdate = [
+  {
+    regex: /https:\/\/thenibble\.substack\.com\/p\/(\d+)\/?/g,
+    replacement: (newNum) => `https://thenibble.substack.com/p/${newNum}`,
+  },
+  {
+    regex: /https:\/\/open\.substack\.com\/pub\/thenibble\/p\/(\d+)\/?/g,
+    replacement: (newNum) =>
+      `https://open.substack.com/pub/thenibble/p/${newNum}`,
+  },
+  {
+    regex: /https:\/\/nibbles\.dev\/p\/(\d+)\/?/g,
+    replacement: (newNum) => `https://nibbles.dev/p/${newNum}`,
+  },
+  {
+    regex: /https:\/\/files\.nibbles\.dev\/covers\/(\d+)\/?/g,
+    replacement: (newNum) => `https://files.nibbles.dev/covers/${newNum}`,
+  },
+  {
+    regex: /https:\/\/files\.nibbles\.dev\/wallpapers\/(\d+)\/?/g,
+    replacement: (newNum) => `https://files.nibbles.dev/wallpapers/${newNum}`,
+  },
+  {
+    regex: /Nibble #(\d+)/g,
+    replacement: (newNum) => `Nibble #${newNum}`,
+  },
+];
+
+// Fetch the latest nibble data
+async function fetchLatestNibble() {
+  const res = await fetch(API_URL);
+  if (!res.ok) {
+    throw new Error(`HTTP error! status: ${res.status}`);
+  }
+  const data = await res.json();
+  const { status, items } = data;
+
+  if (status !== 'ok' || !items?.length) {
+    throw new Error('Failed to fetch RSS feed or no items found');
+  }
+
+  const [latestItem] = items;
+  if (!latestItem) {
+    throw new Error('No latest item found in RSS feed');
+  }
+
+  const { link, title } = latestItem;
+  const match = title.match(/#(\d+)/);
+  if (!match) {
+    throw new Error('Unable to extract nibble number from title');
+  }
+  const latestNumber = match[1];
+
+  return { link, latestNumber };
+}
+
+// Update files with the new link and nibble number
+function updateFiles(newLink, latestNumber) {
+  filesToUpdate.forEach((file) => {
+    try {
+      const data = fs.readFileSync(file, 'utf-8');
+      let updatedContent = data;
+
+      regexesToUpdate.forEach(({ regex, replacement }) => {
+        updatedContent = updatedContent.replaceAll(
+          regex,
+          (match, oldNumber) => {
+            if (oldNumber && oldNumber !== latestNumber) {
+              return replacement(latestNumber);
+            }
+            return match;
+          },
+        );
+      });
+
+      if (data !== updatedContent) {
+        fs.writeFileSync(file, updatedContent, 'utf-8');
+        console.log(`Updated ${file}`);
+      } else {
+        console.log(`No changes needed in ${file}`);
+      }
+    } catch (error) {
+      console.error(`Error updating file ${file}:`, error);
+    }
+  });
+
+  // Update the current link and nibble number
+  try {
+    fs.writeFileSync(
+      'current_link.txt',
+      [newLink, latestNumber].join(DELIMITER),
+      'utf-8',
+    );
+  } catch (error) {
+    console.error('Error updating current_link.txt:', error);
+  }
+}
+
+function getCurrentData() {
+  try {
+    if (fs.existsSync('current_link.txt')) {
+      const data = fs.readFileSync('current_link.txt', 'utf-8').trim();
+      const [link, number] = data.split(DELIMITER);
+      return { link, number };
+    }
+    return { link: '', number: '' };
+  } catch (error) {
+    console.error('Error reading current_link.txt:', error);
+    return { link: '', number: '' };
+  }
+}
+
+(async () => {
+  try {
+    const { link: fetchedLink, latestNumber } = await fetchLatestNibble();
+    const { link: currentLink, number: currentNumber } = getCurrentData();
+
+    if (!fetchedLink || !latestNumber) {
+      throw new Error('Invalid fetched link or nibble number');
+    }
+
+    if (fetchedLink !== currentLink || latestNumber !== currentNumber) {
+      console.log('Link or Nibble number has changed. Updating files...');
+      updateFiles(fetchedLink, latestNumber);
+      console.log('Files updated.');
+    } else {
+      console.log('No changes detected. No action taken.');
+    }
+  } catch (error) {
+    console.error('Error updating Nibble link and number:', error);
+    process.exit(1);
+  }
+})();
